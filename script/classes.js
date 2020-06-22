f7a5ae1f2cfddbc97803d0d7ac538d98bdaae170
Caique Oliveira <caiquerodrigues@id.uff.br>
2020-06-22 12:35:30 -0300

    arquivos js

---
+++ script/classes.js
@@ -0,0 +1,21 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+
+for (let card of cards){
+    card.addEventListener('click', function(){
+        const pageId = card.getAttribute('id')
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.classList.remove('maximize')
+    modalOverlay.querySelector('iframe').src = ``
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+    modalOverlay.classList.add('maximize')
+})
